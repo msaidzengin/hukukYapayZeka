ef7ea1688835eb12ea0418690fbdac616f1be1c0
msaidzengin <m.zengin@etu.edu.tr>
2021-12-02 14:48:08 +0300

    similars code

--- similars.py
+++ similars.py
@@ -4,6 +4,8 @@ import numpy as np
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 
+sbert_model = SentenceTransformer("xlm-r-100langs-bert-base-nli-mean-tokens")
+
 def preprocess(text):
     text = lower(text.translate(str.maketrans("", "", string.punctuation)))
     text = " ".join(text.split())
@@ -22,28 +24,27 @@ def lower(word):
     )
 
 def create_vectors(texts):
-    explanations = [preprocess(i) for i in texts]
-    documents = pd.DataFrame(explanations, columns=["explanations"])
-    sbert_model = SentenceTransformer("xlm-r-100langs-bert-base-nli-mean-tokens")
-    document_embeddings = sbert_model.encode(documents["explanations"])
-    pairwise_similarities = cosine_similarity(document_embeddings)
-    return pairwise_similarities
+    corpus = [preprocess(i) for i in texts]
+    document_embeddings = sbert_model.encode(corpus)
+    return document_embeddings
 
-def find_similars(pairwise_similarities, text, n=5, threshold=0.5):
-    similarities = pairwise_similarities[text]
-    similarities = similarities.reshape(-1)
-    similarities = similarities[similarities > threshold]
-    similarities = similarities[:n]
-    return similarities
+def find_similars(document_embeddings, text, top_k=5, threshold=0.5):
+    text = preprocess(text)
+    query_embedding = sbert_model.encode(text)
+    embeddings = np.insert(document_embeddings, 0, query_embedding, axis=0)
+    pairwise_similarities = cosine_similarity(embeddings)
+    indices = np.argsort(pairwise_similarities[0])[::-1][1:top_k+1] - 1  # -1 because we put query to the first index
+    result = []
+    for idx in indices:
+        result.append(float(pairwise_similarities[0][idx + 1]))
+    return result
 
 with open('kararlar.json', encoding='utf-8') as f:
     kararlar = json.load(f)
 
-kararlar = [y for x,y in kararlar.items()][:10]
-pairwise_similarities = create_vectors(kararlar)
-print(pairwise_similarities)
-find_similars(pairwise_similarities, "karar")
+kararlar = [i for i in kararlar.values()][:10]
+document_embeddings = create_vectors(kararlar)
 
-#text1 = 'Machine learning is the study of computer algorithms that improve automatically through experience. Machine learning algorithms build a mathematical model based on sample data, known as training data. The discipline of machine learning employs various approaches to teach computers to accomplish tasks where no fully satisfactory algorithm is available.'
-#text2 = 'Machine learning is closely related to computational statistics, which focuses on making predictions using computers. The study of mathematical optimization delivers methods, theory and application domains to the field of machine learning.'
-#print(find_similarity(text1, text2))
+text = "Danıştay 12. Daire Başkanlığı         2021/5027 E.  ,  2021/5116 K. İçtihat Metni T.C. D A N I Ş T A Y ONİKİNCİ DAİRE Esas No : 2021/5027 Karar No : 2021/5116 TEMYİZ EDEN (DAVACI) : ... VEKİLİ : Av. … KARŞI TARAF (DAVALI) : … Bakanlığı VEKİLİ : Av. … İSTEMİN KONUSU : ... Bölge İdare Mahkemesi ... İdari Dava Dairesinin ... tarih ve E:…, K:… sayılı kararının temyizen incelenerek bozulması istenilmektedir. YARGILAMA SÜRECİ : Dava konusu istem: Kütahya İli, ... Adliyesi'nde hizmetli olarak görev yapan davacının, ''Silahla Tehdit'' suçundan 1 yıl 8 ay hapis cezasıyla cezalandırılmasına ilişkin ... Asliye Ceza Mahkemesinin … tarihli ve E:…, K:… sayılı kararının 03/10/2019 tarihinde kesinleşmesi üzerine, … Adli Yargı İlk Derece Mahkemesi Adalet Komisyonu Başkanlığının … tarihli ve … sayılı kararı ile getirilen teklif doğrultusunda, 657 sayılı Devlet Memurları Kanunu'nun 48/A-5 ve 98/b maddeleri uyarınca Devlet memurluğu görevine son verilmesine ilişkin … onay tarihli işlemin iptali istenilmiştir. İlk Derece Mahkemesi kararının özeti: … İdare Mahkemesince verilen … tarih ve E:…, K:… sayılı kararla; davacının kasten işlenmiş bir suçtan dolayı 1 yıl 8 ay süre ile kesinleşmiş mahkumiyetinin bulunduğu, bu haliyle 657 sayılı Devlet Memurları Kanunu'nun 48/A-5 maddesinde yer alan memuriyete girişte öngörülen şartlardan birini kaybetmiş olduğu görüldüğünden, aynı Kanun'un 98/b maddesinde yer verilen amir hüküm uyarınca davacının Devlet memurluğu görevine son verilmesine ilişkin dava konusu işlemde hukuka aykırılık bulunmadığı sonucuna ulaşıldığı gerekçesiyle davanın reddine karar verilmiştir. Bölge İdare Mahkemesi kararının özeti: ... Bölge İdare Mahkemesi ... İdari Dava Dairesince; istinaf başvurusuna konu İdare Mahkemesi kararının hukuka ve usule uygun olduğu ve davacı tarafından ileri sürülen iddiaların söz konusu kararın kaldırılmasını gerektirecek nitelikte görülmediği gerekçesiyle, 2577 sayılı İdari Yargılama Usulü Kanunu'nun 45. maddesinin üçüncü fıkrası uyarınca istinaf başvurusunun reddine karar verilmiştir. TEMYİZ EDENİN İDDİALARI : Söz konusu mahkeme kararının kesinleşmediği, Anayasa Mahkemesine ve Avrupa İnsan Hakları Mahkemesine başvuracağı, eksik inceleme sonucunda, hakkında açılan diğer davalarda yaptığı ön ödemeler dikkate alınmadan, hakkında daha önce verilen hükmün açıklanmasının geri bırakılması kararı kaldırılarak hapis cezasına hükmedildiği, mağdur olduğu, aynı olaydan iki kez ceza aldığı, ön ödeme yaptığına ilişkin evrakın ... Asliye Ceza Mahkemesince ilgili yerlere sunulmadığı ileri sürülmektedir. KARŞI TARAFIN SAVUNMASI: Dava konusu işlemin mevzuata uygun olduğu savunulmaktadır. DANIŞTAY TETKİK HÂKİMİ : … DÜŞÜNCESİ : Temyiz isteminin reddi ile usul ve yasaya uygun olan Bölge İdare Mahkemesi kararının onanması gerektiği düşünülmektedir. TÜRK MİLLETİ ADINA Karar veren Danıştay Onikinci Dairesince; Tetkik Hâkiminin açıklamaları dinlendikten ve dosyadaki belgeler incelendikten sonra, 2577 sayılı İdari Yargılama Usulü Kanunu'nun 17. maddesinin ikinci fıkrası uyarınca davacının duruşma istemi yerinde görülmeyerek gereği görüşüldü: HUKUKİ DEĞERLENDİRME: Bölge idare mahkemelerinin nihai kararlarının temyizen bozulması, 2577 sayılı İdari Yargılama Usulü Kanunu'nun 49. maddesinde yer alan sebeplerden birinin varlığı hâlinde mümkündür. Temyizen incelenen karar usul ve hukuka uygun olup, dilekçede ileri sürülen temyiz nedenleri kararın bozulmasını gerektirecek nitelikte görülmemiştir. KARAR SONUCU: Açıklanan nedenlerle; 1. Davacının temyiz isteminin reddine, 2. Davanın yukarıda özetlenen gerekçeyle reddine ilişkin İdare Mahkemesi kararına karşı yapılan istinaf başvurusunun reddi yolundaki temyize konu ... Bölge İdare Mahkemesi ... İdari Dava Dairesinin ... tarih ve E:…, K:… sayılı kararının ONANMASINA, 3. Temyiz giderlerinin istemde bulunan üzerinde bırakılmasına, 4. 2577 sayılı İdari Yargılama Usulü Kanunu'nun 50. maddesi uyarınca, bu onama kararının taraflara tebliğini ve bir örneğinin de ... Bölge İdare Mahkemesi ... İdari Dava Dairesine gönderilmesini teminen dosyanın … İdare Mahkemesine gönderilmesine, 19/10/2021 tarihinde kesin olarak oybirliğiyle karar verildi."
+
+print(find_similars(document_embeddings, text))
